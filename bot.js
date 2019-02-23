4b386f443243ae5fb8db5f9fe1931a228b5414d9
RexyAgar <46576753+RexyAgar@users.noreply.github.com>
2019-02-23 15:42:36 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+
+
+client.on("ready", () => {
+console.log('By : m7md');
+client.user.setPresence({
+  status: 'dnd',
+  game: { 
+     type: .,
+     name: 'هاي يوتيوووب',
+     details: `هاي يوتيوب`,
+     url: 'http://twitch.tv/M7md_Salih',
+     state: `هاي يوتيوب`,
+    application_id: '547435472805298176',
+     assets: {
+        small_image: `377480550207717376`,
+        small_text: 'هاي يوتيوب',
+        large_image: `377480353259978752`,
+        large_text: `هاي يوتيوب` }
+
+  }
+    });
+});
+اقولها
+
+client.login("NTQ3NDM1NDcyODA1Mjk4MTc2.D1LaYQ.RfqLBERqxJKcpvu43swiX0KU4Wk");
